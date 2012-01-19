0c2d75c20319dd6c8c7e56f730a64b3544d83120
Alex Normand <anormnd@gmail.com>
2012-01-19 14:18:47 +0100

    updated index.php

--- get/index.php
+++ get/index.php
@@ -5,23 +5,23 @@ require_once __DIR__ . '/parser.php';
 use Symfony\Component\HttpFoundation\Response;
 
 $getJSON =  function ($query) {
-  return new Response
-  (
-   new Parser($query),			
-   200,
-   array('Content-Type' => 'application/json')
-   );    
+  return new Response(
+     new Parser($query),			
+     200,
+     array('Content-Type' => 'application/json')
+  );    
 };
 
 
 $app = new Silex\Application();
 
-$app->get('/search/{query}', function ($query, \Silex\Application $app) use ($getJSON) {
+$app->get('/search/{query}', function ($query) use ($getJSON, $app) {
     return $getJSON(urlencode($app->escape($query)));
-  });
+});
+
 $app->get('/show/{showid}', function($showid) use ($getJSON) {
     return $getJSON((int) $showid);
-  })
+})
 ->assert('showid', '\d+');
 
 $app->run();
