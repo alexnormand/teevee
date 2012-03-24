b9d33dbfbac383118b9562e252b885b3d173d7b3
Alex Normand <anormnd@gmail.com>
2012-03-25 00:36:15 +0100

    updated back end

--- get/index.php
+++ get/index.php
@@ -1,10 +1,11 @@
 <?php
-
 require_once __DIR__ . '/silex.phar';
 require_once __DIR__ . '/parser.php';
 use Symfony\Component\HttpFoundation\Response;
 
-$getJSON =  function ($query) {
+$app = new Silex\Application();
+
+$app['getJSON'] =  $app->protect(function ($query) {
   $response =  new Response(
 		    new Parser($query),			
 		    200,
@@ -13,23 +14,19 @@ $getJSON =  function ($query) {
     
   $response->setMaxAge(7*60);
   return $response;
-};
-
-
-$app = new Silex\Application();
-
-$app->get('/search/{query}', function ($query) use ($getJSON, $app) {
-    return $getJSON(urlencode($app->escape($query)));
 });
 
+$app->get('/search/{query}', function ($query) use($app) {
+    return $app['getJSON'](urlencode($app->escape($query)));
+});
 
-$app->get('/seasons/{showid}', function($showid) use ($getJSON) {
-    return $getJSON((int) $showid);
+$app->get('/seasons/{showid}', function($showid) use($app) {
+    return $app['getJSON']((int) $showid);
 })
 ->assert('showid', '\d+');
   
-$app->get('/show/{showid}/season/{season}', function($showid, $season) use ($getJSON) {
-    return $getJSON(array('showid' => (int) $showid, 'season' => (int) $season));
+$app->get('/show/{showid}/season/{season}', function($showid, $season) use($app) {
+    return $app['getJSON'](array('showid' => (int) $showid, 'season' => (int) $season));
 })
 ->assert('showid', '\d+')
 ->assert('season', '\d+');
