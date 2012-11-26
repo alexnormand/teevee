37954093d0e148d738139579f9f84c56f361ce73
Alex Normand <anormnd@gmail.com>
2012-11-26 23:46:01 +0100

    improved sliding animation using hardware accelerated css transforms

--- js/main.js
+++ js/main.js
@@ -10,33 +10,23 @@ require.config({
 });
 
 
-require(['require','order!fastclick', 'order!zepto', 'order!underscore', 'order!backbone', 'order!routers/router'], 
+require(['require','order!fastclick', 'order!zepto', 'order!underscore', 'order!backbone', 'order!routers/router'],
 	function(require, FastClick, $, _, Backbone, Router) {
-		
+
 	     // Hides mobile browser's address bar when page is done loading.
 	    $(window).bind('load', function(e) {
 		setTimeout(function() { window.scrollTo(0, 1); }, 1);
 	    });
 
-	    $(window).bind('resize', function() {
-		var s = document.styleSheets[0];
-		s.insertRule("ul {margin-left:" + $('#container').width() + "px;}",
-			 s.cssRules.length);
-	    });
 
 	    //Add FastClick for native-like tapping on smartphones
 	    new FastClick(document.body);
 
-	    // Dynamically set margin-left property for consistent transition speed
-	    // depending on device-width.
-	    var s = document.styleSheets[0];
-	    s.insertRule("ul {margin-left:" + $('#container').width() + "px;}",
-			 s.cssRules.length);
 
-	    var router = new Router;	
+	    var router = new Router;
 	    Backbone.history.start();
-	    	   
+
 	    if(!window.location.hash)
-		router.navigate('/', true);							    
+		router.navigate('/', true);
 });
 
