54efef2fc47f475dbed2a1ba2851b5c9645b93e5
Alex Normand <anormnd@gmail.com>
2012-12-08 23:31:32 +0100

    updated slide animation

--- js/main.js
+++ js/main.js
@@ -13,10 +13,29 @@ require.config({
 require(['require','order!fastclick', 'order!zepto', 'order!underscore', 'order!backbone', 'order!routers/router'],
 	function(require, FastClick, $, _, Backbone, Router) {
 
+			var insertDeviceWidthRule = function() {
+        var s = document.styleSheets[0],
+            containerWidth = $('#container').width(),
+            prefixes = ['-webkit-transform', '-moz-transform', '-ms-transform', '-o-transform', 'transform'],
+            rule;
+
+        prefixes = $.map(prefixes, function (prefix) {
+            return prefix + ': translateX(' + containerWidth + 'px);';
+        });
+
+        rule = 'ul {' + prefixes.join(' ') + '}';
+
+        s.insertRule(rule, s.cssRules.length);
+      };
+
+
 	     // Hides mobile browser's address bar when page is done loading.
 	    $(window).bind('load', function(e) {
-		setTimeout(function() { window.scrollTo(0, 1); }, 1);
+				setTimeout(function() { window.scrollTo(0, 1); }, 1);
 	    });
+
+			$(window).bind('resize', insertDeviceWidthRule);
+      insertDeviceWidthRule();
 
 
 	    //Add FastClick for native-like tapping on smartphones
